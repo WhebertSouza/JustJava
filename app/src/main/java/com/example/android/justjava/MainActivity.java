705ddc942fe2277a09dc1fe7f4da8caab1d73de6
WhebertSouza <whebert@outlook.com>
2016-03-12 11:20:36 -0300

    Removido espaco em branco

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends ActionBarActivity {
 
     int quantity;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
